ca9bd04550d7514b9b9e5ff8a722dd96b71e824e
yotelapago <yotelapago@gmail.com>
2024-11-25 04:31:31 +0000

    UnU

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar";
+import Home from "./components/home";
+import About from "./components/about";
+import Contact from "./components/contact";
 
-function App() {
+const App = () => {
   return (
-    <div>
-      <h1>Hello, world!</h1>
-      <p>This is a simple React page.</p>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
